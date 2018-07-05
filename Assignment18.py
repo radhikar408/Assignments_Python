731c964148cc3fba448b1c1bf5a85a1b0bdd8416
radhikar408 <radhikar408@gmail.com>
2018-07-05 15:44:38 +0530

    d

--- Assignment18.py
+++ Assignment18.py
@@ -1,20 +1,20 @@
 #Ques1--
-import tkinter
-from tkinter import *
-d={"Radhika":1234567892,"jyoti":234567890,"khushi":123456884,"aanchal":55667788,"rahul":9578686,"nihal":8957474785}
-root=Tk()
-#root.resizable(False,False)
-root.geometry("250x250")
-root.minsize(50,50)
-sc=Scrollbar(root)
-sc.pack(side=RIGHT,fill=Y)
-mylist=Listbox(root,yscrollcommand=sc.set)
-for i in d:
-    mylist.insert(END,i)
-mylist.pack(fill=BOTH)
-Scrollbar(mylist,orient="vertical")
-sc.config(command=mylist.yview)
-root.mainloop()
+# import tkinter
+# from tkinter import *
+# d={"Radhika":1234567892,"jyoti":234567890,"khushi":123456884,"aanchal":55667788,"rahul":9578686,"nihal":8957474785}
+# root=Tk()
+# #root.resizable(False,False)
+# root.geometry("250x250")
+# root.minsize(50,50)
+# sc=Scrollbar(root)
+# sc.pack(side=RIGHT,fill=Y)
+# mylist=Listbox(root,yscrollcommand=sc.set)
+# for i in d:
+#     mylist.insert(END,i)
+# mylist.pack(fill=BOTH)
+# Scrollbar(mylist,orient="vertical")
+# sc.config(command=mylist.yview)
+# root.mainloop()
 
 #Q2. In the same tkinter file as created above, create a function to insert items into the dictionary.
 import tkinter
---
+++ assignment19.py
@@ -0,0 +1,45 @@
+#Q.1 - Create a numpy array with 10 elements of the shape(10,1) using np.random and find out the mean of the elements using basic numpy functions.
+import numpy as np
+# a=np.random.random([10,1])
+# print(a)
+# print("mean = ")
+# print(a.mean())
+#Q.2 - Create a numpy array with 20 elements of the shape(20,1) using np.random find the variance and standard deviation of the elements.
+# a=np.random.random([20,1])
+# print(a)
+# print("variance =")
+# print(a.var())
+# print("standard deviation = ")
+# print(a.std())
+#Q.3 - Create a numpy array A of shape(10,20) and B of shape (20,25) using np.random.
+#  Print the matrix which is the matrix multiplication of A and B. The shape of the new matrix should be (10,25).
+#  Using basic numpy math functions only find the sum of all the elements of the new matrix.
+
+# x=np.random.random([10,20])
+# y=np.random.random([20,25])
+# z=np.random.random([10,25])
+# print(x)
+# print(y)
+# print("multiplication of matrix is :")
+# print(np.matmul(x,y))
+# print("the sum of new matrix is :")
+# print(np.sum(z))
+#Q.4 - Create a numpy array A of shape(10,1).Using the basic operations of the numpy array generate an array of shape(10,1) such that
+# each element is the following function applied on each element of A.
+# def show(i):
+#     return(1/(1+np.exp(-a)))
+# a=np.random.random([10,1])
+# b=[]
+# print(a)
+# for i in a:
+#     an=show(i)
+#     b.append(an)
+#     print(np.array(b))
+
+
+
+
+
+
+
+
